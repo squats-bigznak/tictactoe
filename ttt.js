2c220f2c7a09f48ad5e984d832fb7938879d0ed2
squats-bigznak <88125284+squats-bigznak@users.noreply.github.com>
2023-06-27 08:49:08 -0700

    Add basic CSS

--- ttt.js
+++ ttt.js
@@ -1,15 +1,8 @@
-//start with an array 0-8
-//player a chooses a number that hasn't been chosen yet
-//player b chooses a number that hasn't been chosen yet
-//repeat
-//check after each turn for each of 8 win conditions
-//if no win condition by turn 9 tie
 
-//0 1 2
-//3 4 5
-//6 7 8
 
-//win conditions (sorted) - 012, 036, 048, 147, 258, 345, 642, 678
+
+//Gameboard Module with methods for resetting board,
+//updating board, and checking win conditions.
 
 const Gameboard = (() => {
 
@@ -72,6 +65,8 @@ const Gameboard = (() => {
     }
     
   })();
+
+
 
   console.log(Gameboard.updateBoard(0,'x'));
   console.log(Gameboard.updateBoard(1,'o'));
