0d49d1fc462e0133311b33d06d495b6d92192d05
squats-bigznak <88125284+squats-bigznak@users.noreply.github.com>
2023-07-01 20:28:56 -0700

    Finish console version logic

--- ttt.js
+++ ttt.js
@@ -1,20 +1,48 @@
-//need playerFactory
-//functionality will be to toggle entire UI between X and O
+//game loop:
+//load screen 
+//hit restart
+//screen switches to "player X turn"
+//tooltip changes to X
+//X is played
+//win/tie conditions checked
+//screen switches to "player O turn"
+//O is played
+//conditions checked 
+//conditions reached
+//X/O is winner
+//only clickable button is "restart"
 
-const playerFactory = (token) => {
-  const sayHello = () => console.log('Player ' + token + ' hello!');
-  return {sayHello, token};
-};
+const Gamecontroller = (() => {
 
-const playerX = playerFactory('X');
-const playerO = playerFactory('O');
+  //token refers to the player's identity, in this case "X" or "O"
 
-playerX.sayHello();
-playerO.sayHello();
-console.log(playerX.token);
-console.log(playerO.token);
+  const playerFactory = (token) => {
+    const sayHello = () => console.log('Player ' + token + ' hello!');
+    return {sayHello, token};
+  };
 
+  const playerX = playerFactory('X');
+  const playerO = playerFactory('O');
 
+  // playerX.sayHello();
+  // playerO.sayHello();
+  // console.log(playerX.token);
+  // console.log(playerO.token);
+  var currentPlayer = playerX;
+
+  const getCurrentPlayerToken = () => {
+
+    //this switches players every turn
+
+    currentPlayer = currentPlayer === playerX ? playerO : playerX;
+    // console.log(currentPlayer);
+    return (currentPlayer.token);
+    
+  };
+
+  return {getCurrentPlayerToken};
+  
+})();
 
 //Gameboard Module with methods for resetting board,
 //updating board, and checking win conditions.
@@ -24,15 +52,23 @@ const Gameboard = (() => {
     var gameboard = [0,1,2,3,4,5,6,7,8];
     var turncount = 0;
 
-    const updateBoard = (space, player) => {
-      gameboard.splice(space, 1, player);
+    // const getBoard = () => board;
+
+    const playTurn = (space, token) => {
+      if((gameboard[space] == 'X') || (gameboard[space] == 'O')){
+        console.log("spot already chosen");
+      }
+      else{
+      gameboard.splice(space, 1, token);
       turncount++; 
       return gameboard;
+      }
     };  
 
     const checkWin = () =>{
     
       console.log(turncount);
+      console.log(gameboard);
       if (turncount === 9){
         return ("tie!");
       }
@@ -76,21 +112,43 @@ const Gameboard = (() => {
     }
     
     return {
-      updateBoard, checkWin, resetBoard
+      playTurn, checkWin, resetBoard
     }
     
   })();
 
+  //here's where we put it all together
 
+  function playGame(){
+    Gameboard.resetBoard();
 
-  console.log(Gameboard.updateBoard(0,'x'));
-  console.log(Gameboard.updateBoard(1,'o'));
-  console.log(Gameboard.updateBoard(2,'o'));
-  console.log(Gameboard.updateBoard(3,'o'));
-  console.log(Gameboard.updateBoard(4,'o'));
-  console.log(Gameboard.updateBoard(5,'x'));
-  console.log(Gameboard.updateBoard(6,'x'));
-  console.log(Gameboard.updateBoard(7,'x'));
-  console.log(Gameboard.updateBoard(8,'o'));
-  console.log(Gameboard.checkWin());
-  
+    //while loop?
+    do {
+      Gameboard.playTurn(prompt(), Gamecontroller.getCurrentPlayerToken());
+      console.log(Gameboard);
+    }
+    while (Gameboard.checkWin() === undefined);
+    
+    console.log(Gameboard.checkWin());
+  };
+
+ playGame();
+
+  // console.log(Gameboard.playTurn(0,Gamecontroller.getCurrentPlayerToken()));
+  // console.log(Gameboard.playTurn(1,Gamecontroller.getCurrentPlayerToken()));
+  // console.log(Gameboard.playTurn(2,Gamecontroller.getCurrentPlayerToken()));
+  // console.log(Gameboard.playTurn(3,Gamecontroller.getCurrentPlayerToken()));
+
+  // console.log(Gameboard.playTurn(5,'x'));
+  // console.log(Gameboard.playTurn(6,'x'));
+  // console.log(Gameboard.playTurn(7,'x'));
+  // console.log(Gameboard.playTurn(8,'o'));
+  // console.log(Gameboard.checkWin());
+  // Gamecontroller.switchPlayer();
+  // console.log(Gamecontroller.switchPlayer);
+  // console.log(Gamecontroller.switchPlayer.token);
+  // Gamecontroller.switchPlayer();
+  // console.log(Gamecontroller.switchPlayer);
+  // console.log(Gamecontroller.switchPlayer.token);
+  // console.log(Gamecontroller.switchPlayer());
+  // console.log(Gamecontroller.switchPlayer());
